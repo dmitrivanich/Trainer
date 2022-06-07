aae65be6752b314febaa328825ebf2830c1eea54
dmitrivanich <dmitrivanich@gmail.com>
2022-06-07 14:25:52 +0300

    created trainer-js

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,20 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {IonContent, IonHeader, IonPage, IonTitle, IonToolbar} from '@ionic/react';
+import './App.css';
+import {NavLink, useHistory} from 'react-router-dom';
+import { useEffect, useState } from 'react';
+//REDUX
+import { useSelector, useDispatch } from 'react-redux'
+
 
 const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
-  );
+
+    return (
+        <IonPage>
+            <IonContent className="startPage" fullscreen>
+                
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default Home;
